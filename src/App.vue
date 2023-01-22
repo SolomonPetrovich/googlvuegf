51bf39a9a6afd37b6f6d00c0be5d4f80a0dfacb3
Mukhammed-Ali Zholdasbay <80227264+SolomonPetrovich@users.noreply.github.com>
2023-01-22 17:10:02 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <GoogleLoginComponent></GoogleLoginComponent>
+  </div>
+</template>
+
+<script>
+import GoogleLoginComponent from "./components/GoogleLoginComponent";
+
+export default {
+  name: "App",
+  components: {
+    GoogleLoginComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/GoogleLoginComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <GoogleLogin
+      :params="params"
+      :renderParams="renderParams"
+      :onSuccess="onSuccess"
+      :onFailure="onFailure"
+    ></GoogleLogin>
+    <GoogleLogin :params="params" :logoutButton="true">Logout</GoogleLogin>
+  </div>
+</template>
+
+<script>
+import GoogleLogin from "vue-google-login";
+
+export default {
+  data() {
+    return {
+      message: "Google Login",
+      params: {
+        client_id:
+          "465838933882-l86vfb2milaal0kkq7qclnvn6dsslgd8.apps.googleusercontent.com",
+      },
+      renderParams: {
+        width: 250,
+        height: 50,
+        longtitle: true,
+      },
+    };
+  },
+  methods: {
+    onSuccess: function (params) {
+      console.log(params);
+      console.log(params.mc.id_token, "<<<< on success");
+    },
+    onFailure: function (params) {
+      console.log(params, "<<<< on onFailure");
+    },
+  },
+  components: {
+    GoogleLogin,
+  },
+};
+</script>
+
+<style scoped></style>
